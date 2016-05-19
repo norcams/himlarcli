fd85704b316164298e791c9c11f291d2b47df858
Raymond Kristiansen <raymond.kristiansen@it.uib.no>
2016-05-19 15:20:04 +0200

    Add region name to email subject

--- notify_reinstall.py
+++ notify_reinstall.py
@@ -2,10 +2,15 @@
 import sys
 import utils
 from himlarcli.nova import Nova
+from himlarcli.keystone import Keystone
 from himlarcli.notify import Notify
 from email.mime.text import MIMEText
 
 options = utils.get_host_options('Notify users of rebuild host', hosts=1)
+
+# Find region
+keystoneclient = Keystone(options.config)
+region = keystoneclient.get_region()
 
 # Find users
 novaclient = Nova(options.config, options.host[0])
@@ -25,7 +30,7 @@ with open('misc/notify_email.txt', 'r') as body_txt:
     body = body_txt.read()
 
 msg = MIMEText(body)
-msg['Subject'] = 'UH-IaaS: Terminating instance'
+msg['Subject'] = 'UH-IaaS: Terminating instance (%s)' % region
 
 notify = Notify(options.config)
 notify.send_mail(toaddr, msg, debug=0)
