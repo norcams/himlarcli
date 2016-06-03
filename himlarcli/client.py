d010cd19186a5f8325977fda966c4113431782e9
Raymond Kristiansen <raymond.kristiansen@it.uib.no>
2016-06-03 09:44:57 +0200

    Added logging

--- himlarcli/client.py
+++ himlarcli/client.py
@@ -1,4 +1,8 @@
 import sys
+import logging
+import logging.config
+import warnings
+import yaml
 import ConfigParser
 from keystoneclient.auth.identity import v3
 from keystoneclient import session
@@ -12,12 +16,14 @@ class Client(object):
 
     def __init__(self, config_path, debug):
         self.debug = debug
+        self.__setup_logger(debug)
         self.config = ConfigParser.ConfigParser()
         self.config.read(config_path)
         try:
             openstack = self.config._sections['openstack']
         except KeyError as e:
-            print 'ERROR! Could not find section [openstack] in %s' % config_path
+            self.logger.exception('missing [openstack]')
+            self.logger.critical('Could not find section [openstack] in %s', config_path)
             sys.exit(1)
         auth = v3.Password(auth_url=openstack['auth_url'],
                            username=openstack['username'],
@@ -35,3 +41,22 @@ class Client(object):
     @abstractmethod
     def get_client(self):
         pass
+
+    def log(self, msg):
+        self.logger.debug(msg)
+
+    def __setup_logger(self, debug):
+        with open("logging.yaml", 'r') as stream:
+            try:
+                config = yaml.load(stream)
+            except yaml.YAMLError as exc:
+                print(exc)
+        if config:
+            logging.config.dictConfig(config)
+        self.logger = logging.getLogger(__name__)
+        logging.captureWarnings(True)
+        # If debug add verbose console logger only to our logger
+        if (debug):
+            ch = logging.StreamHandler()
+            ch.setLevel(logging.DEBUG)
+            self.logger.addHandler(ch)
