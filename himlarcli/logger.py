733f20047db500593e4a2f21659817a9f4079ec3
Raymond Kristiansen <raymond.kristiansen@it.uib.no>
2016-06-20 16:13:57 +0200

    Updated logger and added test

--- himlarcli/logger.py
+++ himlarcli/logger.py
@@ -1,16 +1,30 @@
+import os
+import sys
 import logging
 import logging.config
 import warnings
 import yaml
 
-def setup_logger(name, debug, configfile="logging.yaml"):
+def setup_logger(name, debug,
+                 log_path = '/opt/himlarcli/',
+                 configfile = 'logging.yaml'):
     with open(configfile, 'r') as stream:
         try:
             config = yaml.load(stream)
         except yaml.YAMLError as exc:
             print(exc)
+
+    # Always use absolute paths
+    if not os.path.isabs(config['handlers']['file']['filename']):
+        config['handlers']['file']['filename'] = log_path + \
+            config['handlers']['file']['filename']
     if config:
-        logging.config.dictConfig(config)
+        try:
+            logging.config.dictConfig(config)
+        except ValueError as e:
+            print e
+            print "Please check your log section in config.ini and logging.yaml"
+            sys.exit(1)
     logger = logging.getLogger(name)
     logging.captureWarnings(True)
     # If debug add verbose console logger
---
+++ tests/logtest.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+import os
+from himlarcli import logger
+
+install_dir = os.environ['VIRTUAL_ENV']
+logger = logger.setup_logger('test',
+                             debug=True,
+                             log_path=install_dir + '/',
+                             configfile=install_dir + '/logging.yaml')
+
+logger.debug('This should be both in logfile and console')
