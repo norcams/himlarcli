bf193246571732c548809f6ee7901a8a137de714
Raymond Kristiansen <raymond.kristiansen@it.uib.no>
2016-06-21 13:00:22 +0200

    Updated ldap client and user list

--- himlarcli/ldapclient.py
+++ himlarcli/ldapclient.py
@@ -5,11 +5,11 @@ import utils
 
 class LdapClient(object):
 
-    def __init__(self, config_path, debug, log=None):
+    def __init__(self, config_path, ldap_config, debug, log=None):
         self.config = utils.get_config(config_path)
         self.logger = utils.get_logger(__name__, self.config, debug, log)
-        server = self.config.get('ldap','server')
-        self.base_dn = self.config.get('ldap', 'base_dn')
+        server = ldap_config['server']
+        self.base_dn = ldap_config['base_dn']
         if not server or not self.base_dn:
             self.logger.critical('Missing config from secton [ldap]')
             sys.exit(1)
--- users.py
+++ users.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import sys
 import utils
 from himlarcli.nova import Nova
 from himlarcli.keystone import Keystone
@@ -9,8 +10,31 @@ options = utils.get_options('Print openstack user stats', hosts=False)
 keystoneclient = Keystone(options.config, options.debug)
 projects = keystoneclient.list_projects('dataporten')
 logger = keystoneclient.get_logger()
-ldapclient = LdapClient(options.config, options.debug, logger)
+
+conf = dict()
+conf['uib'] = {
+    'server': 'ldap.uib.no',
+    'base_dn': 'dc=uib,dc=no',
+    'type': 'employeeType',
+    'org': 'ou'
+}
+conf['uio'] = {
+    'server': 'ldap.uio.no',
+    'base_dn': 'dc=uio,dc=no',
+    'type': 'eduPersonPrimaryAffiliation:',
+    'org': 'ou'
+}
+
+uib = LdapClient(options.config, conf['uib'], options.debug, logger)
+uio = LdapClient(options.config, conf['uio'], options.debug, logger)
+
+# Generate attr list for each location
+for i in conf.keys():
+    conf[i]['attr'] = [conf[i]['type'], conf[i]['org']]
 
 for mail in projects:
     print "---------------- %s ------------------" % mail
-    print ldapclient.get_user(mail)
+    if 'uib' in mail:
+        print uib.get_user(mail, attr=conf['uib']['attr'])
+    elif 'uio' in mail:
+        print uio.get_user(mail, attr=conf['uio']['attr'])
