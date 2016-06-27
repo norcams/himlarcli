9bc01b0ad58741614eb6dad263ceb22599ab27fc
Raymond Kristiansen <raymond.kristiansen@it.uib.no>
2016-06-27 14:34:11 +0200

    Minor cleanup of state

--- state.py
+++ state.py
@@ -10,15 +10,15 @@ desc = 'Update state information'
 actions = ['purge','dump','save', 'start']
 
 options = utils.get_host_action_options(desc, actions, False)
+novaclient = Nova(options.config, debug=options.debug)
+logger = novaclient.get_logger()
+state = State(options.config, debug=options.debug, log=logger)
 
 if options.action[0] == 'save':
-    novaclient = Nova(options.config, debug=options.debug)
     novaclient.save_states()
 if options.action[0] == 'start':
-    novaclient = Nova(options.config, debug=options.debug)
     novaclient.start_instances_from_state()
 elif options.action[0] == 'dump':
-    state = State(options.config, debug=options.debug)
     instances = state.get_instances()
     for i in instances:
         print i
@@ -28,7 +28,7 @@ elif options.action[0] == 'purge':
     answer = raw_input(q)
     if answer.lower() == 'yes':
         state = State(options.config, debug=options.debug)
-        print "We are now purgin all state info"
+        print "We are now purging all state info"
         state.purge()
         state.close()
     else:
