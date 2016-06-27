3337775e573a18c6208e4dd066839d11936a0a35
Raymond Kristiansen <raymond.kristiansen@it.uib.no>
2016-06-27 14:37:30 +0200

    Added domain to setting host and updated log strings

--- himlarcli/nova.py
+++ himlarcli/nova.py
@@ -15,7 +15,7 @@ class Nova(Client):
         """
         super(Nova,self).__init__(config_path, debug, log)
         self.client = novaclient.Client(self.version, session=self.sess)
-        self.host = host
+        self.set_host(host)
 
     def valid_host(self):
         try:
@@ -26,6 +26,13 @@ class Nova(Client):
         return valid
 
     def set_host(self, host):
+        if not host:
+            self.host = None
+            return
+        domain = self.config.get('openstack', 'domain')
+        if domain and not '.' in host:
+            self.logger.debug("=> prepend %s to %s" % (domain, host))
+            host = host + '.' + domain
         self.host = host
 
     def list_instances(self):
@@ -41,7 +48,7 @@ class Nova(Client):
                 ip = '<no public ip>'
             name = i._info['name']
             users[email][name] = { 'ip': ip, 'status': i._info['status']}
-            self.log("list_instances(): instance %s for %s" % (i.name, email))
+            self.logger.debug("=> instance %s for %s" % (i.name, email))
         return users
 
     def list_users(self):
@@ -53,9 +60,9 @@ class Nova(Client):
             # avoid system users in list
             if "@" in email:
                 emails.add(email.lower())
-                self.log("list_users(): add %s to email list" % email)
+                self.logger.debug("=> add %s to email list" % email)
             else:
-                self.log("list_users(): drop %s from email list" % email)
+                self.logger.debug("=> drop %s from email list" % email)
         return list(emails)
 
     def save_states(self):
@@ -93,10 +100,10 @@ class Nova(Client):
             if instance._info['status'] != 'ACTIVE':
                 instance.start()
                 count += 1
-                self.log("start_instances_from_state(): starting %s" % i[1])
+                self.logger.debug("=> starting %s" % i[1])
             else:
-                self.log("start_instances_from_state(): already running %s" % i[1])
-        print "run start on %s instances with previous state ACTIVE" % (count)
+                self.logger.debug("=> already running %s" % i[1])
+        print "Run start on %s instances with previous state ACTIVE" % (count)
 
     def delete_instances(self, state='SHUTOFF'):
         """ Delete all instances on a host with one state
@@ -119,12 +126,12 @@ class Nova(Client):
                 getattr(i, action)()
                 count += 1
                 self.instances[i.name] = i.id
-                self.log('Change status to %s on %s' % (action, i.name))
+                self.logger.debug('=> change status to %s on %s' % (action, i.name))
             else:
                 print i.id
-                self.log('%s had not status %s' % (i.name, state))
+                self.logger.debug('=> %s had not status %s' % (i.name, state))
 
-        print "run %s on %s instances with state %s" % (action, count, state)
+        print "Run %s on %s instances with state %s" % (action, count, state)
 
     def __get_instances(self):
         if not self.valid_host():
