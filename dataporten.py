21740e58993c26865c0ff5ea9f74a1ca7f1b7d61
Raymond Kristiansen <raymond.kristiansen@it.uib.no>
2016-09-12 15:27:53 +0200

    Bugfix last commit

--- dataporten.py
+++ dataporten.py
@@ -5,19 +5,18 @@ import utils
 from himlarcli.keystone import Keystone
 from himlarcli import utils as himutils
 
-rules = [{
-    "local": [{
-        "user": { "name": "{1}","id": "{0}" },
-        "group": { "name": "{0}-group", "domain": { "id": domain } } }],
-    "remote": [{ "type": "OIDC-email" }, { "type": "OIDC-sub" }]
-}]
-
 options = utils.get_options('Setup dataporten openid mapping',
                              hosts=0, dry_run=True)
 ksclient = Keystone(options.config, debug=options.debug)
 
 # Domain should be create from hieradata
 domain = ksclient.get_domain_id('dataporten')
+rules = [{
+    "local": [{
+        "user": { "name": "{1}","id": "{0}" },
+        "group": { "name": "{0}-group", "domain": { "id": domain } } }],
+    "remote": [{ "type": "OIDC-email" }, { "type": "OIDC-sub" }]
+}]
 
 # Create provider, mapping and container to connect them
 ksclient.set_identity_provider('dataporten', 'https://auth.dataporten.no')
