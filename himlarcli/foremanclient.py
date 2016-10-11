77eadf57d3327010189e616467de8210e3e26ba2
Raymond Kristiansen <raymond.kristiansen@it.uib.no>
2016-10-11 15:27:21 +0200

    Rework of node script

--- himlarcli/foremanclient.py
+++ himlarcli/foremanclient.py
@@ -20,6 +20,13 @@ class Client(object):
     def get_client(self):
         return self.foreman
 
+    def get_compute_resources(self):
+        resources = self.foreman.index_computeresources()
+        found_resources = dict({})
+        for r in resources['results']:
+            found_resources[r['name']] = r['id']
+        return found_resources
+
     def get_host(self, host):
         host = self.__set_host(host)
         return self.foreman.show_hosts(id=host)
@@ -43,6 +50,36 @@ class Client(object):
         result = self.foreman.create_host(host)
         self.logger.debug('=> host created: %s' % result)
 
+    def create_node(self, name, node_data, region, dry_run=False):
+        if self.get_host(name):
+            self.logger.debug('=> node %s exist, dropping create' % name)
+            return
+        found_resources = self.get_compute_resources()
+        host = dict()
+        host['name'] = name
+        host['build'] = self.__get_node_data('build', node_data, '1')
+        host['hostgroup_id'] = self.__get_node_data('hostgroup', node_data, '1')
+        host['compute_profile_id'] = self.__get_node_data('compute_profile', node_data, '1')
+        if 'mac' in node_data:
+            host['mac'] = node_data['mac']
+        if 'compute_resource' in node_data:
+            compute_resource = '%s-%s' % (region, node_data['compute_resource'])
+            if compute_resource in found_resources:
+                host['compute_resource_id'] = found_resources[compute_resource]
+            else:
+                self.logger.critical('=> compute resource %s not found' % compute_resource)
+                return
+        elif 'mac' not in node_data:
+            self.logger.critical('=> mac or compute resource are mandatory for %s' % name)
+            return
+        if not dry_run:
+            result = self.foreman.create_hosts(host)
+            if 'mac' not in node_data:
+                self.foreman.hosts.power(id=result['name'], power_action='start')
+            self.logger.debug('=> create host %s' % result)
+        else:
+            self.logger.debug('=> dry run: host config %s' % host)
+
     def __set_host(self, host):
         if not host:
             self.host = None
@@ -52,6 +89,12 @@ class Client(object):
             self.logger.debug("=> prepend %s to %s" % (domain, host))
             host = host + '.' + domain
         return host
+
+    def __get_node_data(self, var, node_data, default=None):
+        if var in node_data:
+            return node_data[var]
+        else:
+            return default
 
     def _print_attr():
         attr = self.foreman.__dict__
--- utils.py
+++ utils.py
@@ -62,3 +62,39 @@ def get_host_action_options(desc, actions, hosts=True, config=True, debug=True):
                         nargs=1,
                         help='|'.join(actions))
     return parser.parse_args()
+
+def get_node_action_options(desc, actions, nodes=True, dry_run=False, config=True, debug=True):
+    parser = argparse.ArgumentParser(description=desc)
+    if debug:
+        parser.add_argument('-d',
+                            dest='debug',
+                            action='store_const',
+                            const=True,
+                            default=False,
+                            help='verbose debug mode')
+    if config:
+        parser.add_argument('-c',
+                            dest='config',
+                            metavar='config.ini',
+                            action='store',
+                            default='/etc/himlarcli/config.ini',
+                            help='path to ini file with config')
+    if dry_run:
+        parser.add_argument('--dry-run',
+                            dest='dry_run',
+                            action='store_const',
+                            const=True,
+                            default=False,
+                            help='dry run script')
+    if nodes:
+        parser.add_argument('node',
+                            metavar='hostname',
+                            action='store',
+                            help='node hostname')
+    parser.add_argument('action',
+                        metavar='actions',
+                        choices=actions,
+                        type=str,
+                        nargs=1,
+                        help='|'.join(actions))
+    return parser.parse_args()
