171f2573f128076f1ed722f553c86f79274e443b
Raymond Kristiansen <raymond.kristiansen@it.uib.no>
2016-11-17 16:26:34 +0100

    Updated stats script to collect from different regions

--- stats.py
+++ stats.py
@@ -1,32 +1,47 @@
 #!/usr/bin/env python
 
+import sys
 import pprint
 import utils
 import statsd
+from himlarcli import utils as himutils
 from himlarcli.nova import Nova
 from himlarcli.keystone import Keystone
 
 options = utils.get_options('Print openstack location stats', hosts=False)
 
+# Project
 keystoneclient = Keystone(options.config, options.debug)
 projects_count = keystoneclient.get_project_count('dataporten')
-
-novaclient = Nova(options.config, debug=options.debug)
-novastats = novaclient.get_stats('dataporten')
+logger = keystoneclient.get_logger()
 
 stats = dict()
 stats['projects'] = {}
 stats['instances'] = {}
-stats['projects']['count'] = projects_count
-stats['instances']['count'] = novastats['count']
+stats['projects'][keystoneclient.region] = {}
+stats['projects'][keystoneclient.region]['count'] = projects_count
 
-pp = pprint.PrettyPrinter(indent=2)
-pp.pprint(stats)
+server = keystoneclient.get_config('statsd', 'server')
+port = keystoneclient.get_config('statsd', 'port')
 
-server = novaclient.get_config('statsd', 'server')
-port = novaclient.get_config('statsd', 'port')
+# Regions
+regions = himutils.load_region_config('config/stats',
+                                      region=keystoneclient.region,
+                                      log=logger)
 
-prefix = 'uh-iaas.%s' % keystoneclient.region
+# Instances
+for name, info in sorted(regions['regions'].iteritems()):
+    logger.debug('=> count region %s' % name)
+    novaclient = Nova(options.config, debug=options.debug, region=name)
+    novastats = novaclient.get_stats(info['domain'])
+    stats['instances'][name] = {}
+    stats['instances'][name]['count'] = novastats['count']
+
+prefix = 'uh-iaas'
 c = statsd.StatsClient(server, port, prefix=prefix)
-for name, s in stats.iteritems():
-    c.gauge(name, s['count'])
+for t, s in stats.iteritems():
+    for r, d in s.iteritems():
+        name = '%s.%s' % (r, t)
+        count = d['count']
+        print '%s = %s' % (name, count)
+        c.gauge(name, count)
