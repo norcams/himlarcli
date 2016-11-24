84f42d8ef6bd8e5a7a7c942dcf5f927944437f2c
Raymond Kristiansen <raymond.kristiansen@it.uib.no>
2016-11-24 15:25:57 +0100

    Added generic arg parse action option

--- utils.py
+++ utils.py
@@ -3,6 +3,8 @@ import sys
 import os.path
 import argparse
 
+
+
 def get_options(desc, config=True, debug=True, dry_run=False, hosts='+'):
     parser = argparse.ArgumentParser(description=desc)
     if debug:
@@ -93,6 +95,58 @@ def get_node_action_options(desc, actions, nodes=True, dry_run=False, config=Tru
                             action='store',
                             help='nodename (mandatory for some actions)',
                             default=None)
+    parser.add_argument('action',
+                        metavar='actions',
+                        choices=actions,
+                        type=str,
+                        nargs=1,
+                        help='|'.join(actions))
+    return parser.parse_args()
+
+def get_action_options(desc, actions, dry_run=False, config=True, debug=True, opt_args={}):
+    parser = argparse.ArgumentParser(description=desc)
+    if debug:
+        parser.add_argument('-d',
+                            dest='debug',
+                            action='store_const',
+                            const=True,
+                            default=False,
+                            help='verbose debug mode')
+    if config:
+        parser.add_argument('-c',
+                            dest='config',
+                            metavar='config.ini',
+                            action='store',
+                            default='/etc/himlarcli/config.ini',
+                            help='path to ini file with config')
+    if dry_run:
+        parser.add_argument('--dry-run',
+                            dest='dry_run',
+                            action='store_const',
+                            const=True,
+                            default=False,
+                            help='dry run script')
+    for name, arg in opt_args.iteritems():
+        if not 'dest' in arg:
+            print 'missing dest in opt_args'
+            continue
+        if not 'default' in arg:
+            arg['default'] = False
+        if not 'action' in arg:
+            arg['action'] = 'store'
+        if not 'help' in arg:
+            arg['help'] = arg['dest']
+        if not 'metavar' in arg:
+            arg['metavar'] = arg['dest']
+        if not 'const' in arg:
+            arg['const'] = None
+        parser.add_argument(name,
+                            dest=arg['dest'],
+                            action=arg['action'],
+                            default=arg['default'],
+                            metavar=arg['metavar'],
+                            const=arg['const'],
+                            help=arg['help'])
     parser.add_argument('action',
                         metavar='actions',
                         choices=actions,
