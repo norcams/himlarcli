ba25189689701a69442cd03c160f6a61d3a80dee
Raymond Kristiansen <raymond.kristiansen@it.uib.no>
2017-04-04 13:35:41 +0200

    Rewriten state class for image status

--- himlarcli/state.py
+++ himlarcli/state.py
@@ -1,35 +1,99 @@
-import sys
 import ConfigParser
 import sqlite3
-import utils
+import sys
+from himlarcli import utils
+
 
 class State(object):
 
+    ACTIVE_TABLE = 'active'
+    IMAGE_TABLE = 'images'
+    TABLES = dict()
+    TABLES[IMAGE_TABLE] = ('CREATE TABLE IF NOT EXISTS %s ('
+                           'id TEXT PRIMARY KEY, '
+                           'name TEXT, '
+                           'status TEXT, '
+                           'region TEXT)') % IMAGE_TABLE
+    TABLES[ACTIVE_TABLE] = ('CREATE TABLE IF NOT EXISTS %s ('
+                            'id TEXT PRIMARY KEY, '
+                            'name TEXT, '
+                            'state TEXT, '
+                            'host TEXT)') % ACTIVE_TABLE
     conn = None
-    table = 'Active'
 
     def __init__(self, config_path, debug, log=False):
+        self.config_path = config_path
         self.config = utils.get_config(config_path)
         self.logger = utils.get_logger(__name__, self.config, debug, log)
+        self.logger.debug('=> config file: %s', config_path)
         self.debug = debug
-        try:
-            self.state = self.config._sections['state']
-        except KeyError as e:
-            self.logger.exception('missing [state]')
-            self.logger.critical('Could not find section [state] in %s', config_path)
-            sys.exit(1)
+        self.db = self.__get_config('state', 'db')
+        self.connect()
+        # Make sure all tables exists
+        self.__create_tables()
 
+    """
+    Connect to db
+    """
     def connect(self):
         try:
-            self.conn = sqlite3.connect(self.state['db'])
+            self.conn = sqlite3.connect(self.db)
             cur = self.conn.cursor()
             cur.execute('SELECT SQLITE_VERSION()')
             data = cur.fetchone()
-            self.logger.debug("=> sqlite3 version %s" % data)
-            self.logger.debug("=> connected to %s" % self.state['db'])
+            self.logger.debug("=> sqlite3 version %s", data)
+            self.logger.debug("=> connected to %s", self.db)
         except sqlite3.Error as e:
-            self.logger.critical("ERROR %s" % e.args[0])
+            self.logger.critical("ERROR %s", e.args[0])
             sys.exit(1)
+
+    def close(self):
+        self.conn.close()
+
+    def purge(self, table_name):
+        self.__purge_table(name=table_name)
+        self.__create_table(name=table_name)
+
+    def insert(self, table, **kwargs):
+        cur = self.conn.cursor()
+        if 'id' in kwargs:
+            sql = "SELECT count(*) FROM %s WHERE id = '%s';" % (table, kwargs['id'])
+            cur.execute(sql)
+            if cur.fetchone()[0] > 0:
+                self.logger.debug("=> id %s exits in table %s", kwargs['id'], table)
+                return
+        sql = ('INSERT')
+        columns = []
+        values = []
+        for key, value in kwargs.iteritems():
+            columns.append(key)
+            if isinstance(value, basestring):
+                values.append("'%s'" % value)
+            else:
+                values.append(value)
+        sql = ('INSERT INTO %s (%s) VALUES (%s)') % (
+            table, ', '.join(str(x) for x in columns),
+            ', '.join(str(x) for x in values))
+
+        self.logger.debug("=> SQL=%s", sql)
+        cur.execute(sql)
+        self.conn.commit()
+
+    def fetch(self, table, columns='*', **kwargs):
+        sql = ('SELECT %s FROM %s') % (columns, table)
+        clause = []
+        for key, value in kwargs.iteritems():
+            if isinstance(value, basestring):
+                clause.append("%s='%s'" % (key, value))
+            else:
+                clause.append("%s=%s" % (key, value))
+        if clause:
+            sql += (' WHERE ')
+            sql += ' and '.join(str(x) for x in clause)
+        cur = self.conn.cursor()
+        self.logger.debug("=> SQL=%s", sql)
+        cur.execute(sql)
+        return cur.fetchall()
 
     def add_active(self, instances):
         if not self.conn:
@@ -85,19 +149,14 @@ class State(object):
             self.logger.debug("%s" % str(row))
         return rows
 
-    def purge(self):
-        if not self.conn:
-            self.connect()
-
-        sql = "DROP TABLE IF EXISTS %s" % self.table
-        cur = self.conn.cursor()
-        cur.execute(sql)
-        self.logger.debug("=> %s" % sql)
-        self._create_active_table()
-
-    def close(self):
-        if self.conn:
-            self.conn.close()
+#    def purge(self):
+#        if not self.conn:
+#            self.connect()
+#        sql = "DROP TABLE IF EXISTS %s" % self.table
+#        cur = self.conn.cursor()
+#        cur.execute(sql)
+#        self.logger.debug("=> %s" % sql)
+#        self._create_active_table()
 
     def _create_active_table(self):
         with self.conn:
@@ -108,3 +167,49 @@ class State(object):
             cur = self.conn.cursor()
             cur.execute(sql)
             self.logger.debug("=> %s" % sql)
+
+    """
+    Purge all tables
+    """
+    def __purge_tables(self):
+        for name in self.TABLES.iterkeys():
+            self.__purge_table(name=name)
+
+    """
+    Purge table if it exists
+    """
+    def __purge_table(self, name):
+        cur = self.conn.cursor()
+        sql = "DROP TABLE IF EXISTS %s" % name
+        cur.execute(sql)
+        self.logger.debug('=> purge table for %s', name)
+
+    """
+    Create all new tables if they do not exists
+    """
+    def __create_tables(self):
+        for name in self.TABLES.iterkeys():
+            self.__create_table(name=name)
+
+    """
+    Create new table if it does not exists
+    """
+    def __create_table(self, name):
+        table = self.TABLES[name]
+        cur = self.conn.cursor()
+        cur.execute(table)
+        self.logger.debug('=> create new table for %s', name)
+
+    """
+    Fetch config from config file
+    """
+    def __get_config(self, section, option):
+        try:
+            value = self.config.get(section, option)
+            return value
+        except ConfigParser.NoOptionError:
+            self.logger.debug('=> config file section [%s] missing option %s',
+                              section, option)
+        except ConfigParser.NoSectionError:
+            self.logger.debug('=> config file missing section %s', section)
+        return None
