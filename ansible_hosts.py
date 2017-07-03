ee1d1696c4066955c2d321f266c4eca60dc4f108
Raymond Kristiansen <raymond.kristiansen@it.uib.no>
2017-07-03 11:33:07 +0200

    Cleanup for pylint

--- ansible_hosts.py
+++ ansible_hosts.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 import utils
-import sys
 import ConfigParser
 from himlarcli.foremanclient import Client
 
@@ -12,30 +11,30 @@ foreman = Client(options.config, options.debug)
 hosts = foreman.get_hosts('*')
 hostlist = dict()
 for host in hosts['results']:
-  hostname = host['name'].split('.')[0]
-  loc,role,id = hostname.split('-')
-  group = "%s-%s" % (loc, role)
-  if not group in hostlist:
-    hostlist[group] = []
-  hostlist[group].append(hostname)
+    hostname = host['name'].split('.')[0]
+    loc, role, num = hostname.split('-')
+    group = "%s-%s" % (loc, role)
+    if not group in hostlist:
+        hostlist[group] = []
+    hostlist[group].append(hostname)
 
 children = "%s:children" % loc
 filename = "hostfile.%s" % loc
 nodes = "%s-nodes:children" % loc
-physical = { 'storage', 'compute', 'controller', 'leaf', 'login' }
+physical = {'storage', 'compute', 'controller', 'leaf', 'login'}
 
 parser = ConfigParser.ConfigParser(allow_no_value=True)
 parser.add_section(children)
 parser.add_section(nodes)
 
-for section,hosts in sorted(hostlist.iteritems()):
-  loc, role = section.split('-')
-  parser.set(children, section)
-  parser.add_section(section)
-  if role not in physical:
-    parser.set(nodes, section)
-  for host in hosts:
-    parser.set(section, host)
+for section, hosts in sorted(hostlist.iteritems()):
+    loc, role = section.split('-')
+    parser.set(children, section)
+    parser.add_section(section)
+    if role not in physical:
+        parser.set(nodes, section)
+    for host in hosts:
+        parser.set(section, host)
 
 with open(filename, 'w') as f:
-      parser.write(f)
+    parser.write(f)
