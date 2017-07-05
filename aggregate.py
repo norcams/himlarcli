c3098b6b612dca2bb981cff2ab1f7bb4a2eb5a9b
Raymond Kristiansen <raymond.kristiansen@it.uib.no>
2017-07-05 08:20:11 +0200

    Print id in aggregate instance list

--- aggregate.py
+++ aggregate.py
@@ -59,7 +59,7 @@ def action_instances():
             stats[i.status] += 1
         else:
             stats[i.status] = 1
-        print '%s (status=%s)' % (unicode(i.name), i.status)
+        print '%s %s (status=%s)' % (i.id, unicode(i.name), i.status)
     print "\nSTATUS:"
     print "======="
     pp.pprint(stats)
