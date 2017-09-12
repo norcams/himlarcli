3f0c8e228df61469277692f9d54d68e5ebd9c5df
Raymond Kristiansen <raymond.kristiansen@it.uib.no>
2017-09-12 13:36:02 +0200

    Added options to change default config file for quota

--- quota.py
+++ quota.py
@@ -43,17 +43,19 @@ def action_show():
 
 def action_update():
     dry_run_txt = 'DRY-RUN: ' if options.dry_run else ''
-    defaults = himutils.load_config('config/quotas/default.yaml', logger)
+    defaults = himutils.load_config('config/quotas/%s' % options.quota_config, logger)
+    if not defaults:
+        himutils.sys_error('No default quotas found in config/quota/%s' % options.quota_config)
     for region in regions:
         novaclient = Nova(options.config, debug=options.debug, log=logger, region=region)
         cinderclient = Cinder(options.config, debug=options.debug, log=logger, region=region)
         neutronclient = Neutron(options.config, debug=options.debug, log=logger, region=region)
         components = {'nova': novaclient, 'cinder': cinderclient, 'neutron': neutronclient}
         for comp, client in components.iteritems():
-            if comp not in defaults:
-                logger.debug('=> could not find quota for %s in default.yaml' % comp)
-                continue
             if options.service != 'all' and comp != options.service:
+                continue
+            if comp not in defaults:
+                logger.debug('=> could not find quota for %s in config' % comp)
                 continue
             if hasattr(client, 'get_quota_class'):
                 current = getattr(client, 'get_quota_class')()
@@ -68,11 +70,10 @@ def action_update():
                     logger.debug("=> %sUpdated %s: from %s to %s in %s" %
                                  (dry_run_txt, k, current[k], v, region))
                     updates[k] = v
-            if updates:
-                if not options.dry_run:
-                    result = getattr(client, 'update_quota_class')(updates=updates)
-                    logger.debug('=> %s' % result)
-            else:
+            if updates and not options.dry_run:
+                result = getattr(client, 'update_quota_class')(updates=updates)
+                logger.debug('=> %s' % result)
+            elif not updates:
                 logger.debug('=> no need to update default quota for %s in %s' % (comp, region))
 
 # Run local function with the same name as the action
