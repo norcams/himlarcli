f1b72b2ca1ccc601e4dc6e4be4cc298c6b3d6a79
Raymond Kristiansen <raymond.kristiansen@it.uib.no>
2017-09-28 12:28:28 +0200

    Updated demo script with notify

--- demo.py
+++ demo.py
@@ -2,6 +2,7 @@
 from himlarcli.keystone import Keystone
 from himlarcli.parser import Parser
 from himlarcli.printer import Printer
+from himlarcli.notify import Notify
 from himlarcli import utils as himutils
 
 himutils.is_virtual_env()
@@ -14,6 +15,7 @@ ksclient = Keystone(options.config, debug=options.debug)
 ksclient.set_dry_run(options.dry_run)
 logger = ksclient.get_logger()
 
+
 def action_create():
     users = ksclient.get_users(domain=options.domain)
     printer.output_dict({'header': 'Created demo project'})
@@ -23,10 +25,19 @@ def action_create():
             logger.debug('=> %s user missing email' % user.name)
             continue
         search_filter = dict({'type': 'demo'})
-        projects = ksclient.get_user_projects(email=user.email,
+        projects = ksclient.get_user_projects(email=user.email.lower(),
                                               domain=options.domain,
                                               **search_filter)
         if not projects:
+            project_name = user.email.lower().replace('@', '.')
+            project_name = 'DEMO-%s' % project_name
+            desc = ('Personal demo project for %s. Resources might be '
+                    'terminated at any time.' % user.email.lower())
+            ksclient.create_project(domain=options.domain,
+                                    project_name=project_name,
+                                    description=desc,
+                                    type='demo',
+                                    admin=user.email.lower())
             output_user = {
                 'id': user.id,
                 'name': user.name,
@@ -34,6 +45,59 @@ def action_create():
             count += 1
             printer.output_dict(output_user, sort=True, one_line=True)
     printer.output_dict({'Users without demo project': count})
+
+def action_notify():
+    question = 'Send mail to all users about demo projects'
+    if options.dry_run:
+        question = 'DRY-RUN: %s' % question
+    if not himutils.confirm_action(question):
+        return
+
+    projects = ksclient.get_projects(domain=options.domain)
+    count = 0
+    for project in projects:
+        if hasattr(project, 'notify'):
+            himutils.sys_error('user %s allready notified %s' % (project.name, project.notify), 0)
+            continue
+        found = False
+        if hasattr(project, 'type') and project.type == 'personal':
+            print "%s (new personal project)" % project.name
+            count += 1
+            found = True
+        elif '@' in project.name:
+            print "%s (old personal project)"
+            count += 1
+            found = True
+        if found:
+            if not '@' in project.name:
+                himutils.sys_error('unable to find email for project %s' % project.name, 0)
+                continue
+            user = ksclient.get_user_by_email(project.name, 'api', options.domain)
+            if not user:
+                himutils.sys_error('unable to find user for project %s' % project.name, 0)
+                continue
+            search_filter = dict({'type': 'demo'})
+            demo_project = ksclient.get_user_projects(email=user.email.lower(),
+                                                      domain=options.domain,
+                                                      **search_filter)
+            demo_project = demo_project[0] if demo_project else None
+            if not demo_project:
+                himutils.sys_error('unable to find demo project for %s' % user.name, 0)
+                continue
+            delete_date = '2017-10-31'
+            ksclient.update_project(project_id=project.id, notify=delete_date)
+            #mapping = dict(region=region.upper(), project=project.name)
+            mapping = {'personal': project.name, 'date': delete_date, 'demo': demo_project.name}
+            body_content = himutils.load_template(inputfile='misc/notify_demo.txt',
+                                                  mapping=mapping,
+                                                  log=logger)
+            subject = ('UH-IaaS: Your personal project will be deleted')
+            notify = Notify(options.config, debug=False, log=logger)
+            notify.set_dry_run(options.dry_run)
+            notify.mail_user(body_content, subject, user.email)
+            notify.close()
+            print body_content
+    printer.output_dict({'Personal projects': count})
 
 def action_validate():
     valid_projects = ['demo', 'research', 'education', 'admin', 'test']
