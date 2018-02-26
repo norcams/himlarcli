59678fa0bbff09969993a6c2c42fbe7827a968a3
Raymond Kristiansen <raymond.kristiansen@it.uib.no>
2018-02-26 15:14:43 +0100

    Added object script

---
+++ object.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+from himlarcli import tests as tests
+tests.is_virtual_env()
+
+from himlarcli.keystone import Keystone
+from himlarcli.nova import Nova
+from himlarcli.cinder import Cinder
+from himlarcli.parser import Parser
+from himlarcli.printer import Printer
+from himlarcli import utils as himutils
+
+parser = Parser()
+options = parser.parse_args()
+printer = Printer(options.format)
+
+ksclient = Keystone(options.config, debug=options.debug)
+ksclient.set_dry_run(options.dry_run)
+logger = ksclient.get_logger()
+
+if hasattr(options, 'region'):
+    regions = ksclient.find_regions(region_name=options.region)
+else:
+    regions = ksclient.find_regions()
+
+if not regions:
+    himutils.sys_error('No valid regions found!')
+
+def action_grant():
+    project = ksclient.get_project_by_name(project_name=options.project, domain=options.domain)
+    if not project:
+        himutils.sys_error('No project found with name %s' % options.project)
+    users = ksclient.get_users(domain=options.domain, project=project.id)
+    for user in users:
+        ksclient.grant_role(email=user.email, project_name=project.name, role='object')
+
+def action_revoke():
+    print "TODO"
+
+# Run local function with the same name as the action
+action = locals().get('action_' + options.action)
+if not action:
+    himutils.sys_error("Function action_%s() not implemented" % options.action)
+action()
