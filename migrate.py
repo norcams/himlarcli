1674fd0235c39834f2871b85a84bdf1d2f102ba6
Raymond Kristiansen <raymond.kristiansen@it.uib.no>
2018-05-03 17:22:38 +0200

    Added check to see if host is down before evacuate can action be run

--- migrate.py
+++ migrate.py
@@ -69,6 +69,9 @@ def action_migrate():
             break
 
 def action_evacuate():
+    source_host = nc.get_host(source)
+    if source_host.state != 'down':
+        himutils.sys_error('Evacuate failed. Source host need to be down!')
     # Check that there are other valid hosts in the same aggregate
     hosts = nc.get_aggregate_hosts(options.aggregate)
     found_enabled = list()
