d79af2dae9433f8d18b4761b14336fbd58b979d4
Trond Hasle Amundsen <t.h.amundsen@usit.uio.no>
2018-07-20 20:00:53 +0200

    add code to manage dns blacklists

--- himlarcli/designate.py
+++ himlarcli/designate.py
@@ -8,7 +8,7 @@ class Designate(Client):
     SERVICE_TYPE = 'dns'
 
     def __init__(self, config_path, debug=False, log=None, region=None):
-        """ Create a new designate client to manaage DNS
+        """ Create a new designate client to manage DNS
         `**config_path`` path to ini file with config
         `**log` logger object
         """
@@ -21,8 +21,48 @@ class Designate(Client):
         """ Return the designate client """
         return self.client
 
+    #-------------------------------------------------------------------
+    # Blacklists functions
+    #-------------------------------------------------------------------
+    def list_blacklists(self):
+        result = self.client.blacklists.list()
+        if result:
+            return result
+            #return result.to_dict()
+        return dict()
 
-    # Hello world example
-    def create_domain(self, name, email):
-        zone = self.client.zones.create(name, email=email)
-        return zone
+    def create_blacklist(self, pattern, description):
+        bl = self.client.blacklists.create(pattern=pattern, description=description)
+        return bl
+
+    def delete_blacklist(self, blacklist_id):
+        bl = self.client.blacklists.delete(blacklist_id=blacklist_id)
+        return bl
+
+    def update_blacklist(self, blacklist_id, values):
+        bl = self.client.blacklists.update(blacklist_id=blacklist_id, values=values)
+        return bl
+
+    #-------------------------------------------------------------------
+    # Zones functions
+    #-------------------------------------------------------------------
+    def list_zones(self):
+        result = self.client.zones.list()
+        if result:
+            return result
+            #return result.to_dict()
+        return dict()
+
+    #-------------------------------------------------------------------
+    # TLDs functions
+    #-------------------------------------------------------------------
+    def list_tlds(self):
+        result = self.client.tlds.list()
+        if result:
+            return result
+            #return result.to_dict()
+        return dict()
+
+    def create_tld(self, name, description):
+        tld = self.client.tlds.create(name, description)
+        return tld
