fe636335726c0118b25c86e99f99ba474204d2db
Anders Vaage <anders@unix.uib.no>
2018-07-27 16:04:24 +0200

    add twitter action to publish_status

--- publish_status.py
+++ publish_status.py
@@ -3,6 +3,7 @@ import time
 from himlarcli.parser import Parser
 from himlarcli.printer import Printer
 from himlarcli.slack import Slack
+from himlarcli.twitter import Twitter
 from himlarcli import utils as himutils
 
 himutils.is_virtual_env()
@@ -12,12 +13,22 @@ options = parser.parse_args()
 printer = Printer(options.format)
 
 def action_slack():
+    message = parse_template()
+    slack = Slack(options.config, debug=options.debug)
+    slack.set_dry_run(options.dry_run)
+    slack.publish_slack(message)
+
+def action_twitter():
+    message = parse_template()
+    twitter = Twitter(options.config, debug=options.debug)
+    twitter.set_dry_run(options.dry_run)
+    twitter.publish_twitter(message)
+
+def parse_template():
     msg_content = himutils.load_template(inputfile=options.template,
                                          mapping={})
     stripped_msg = msg_content.rstrip('\n')
-    slack = Slack(options.config, debug=options.debug)
-    slack.set_dry_run(options.dry_run)
-    slack.publish_slack(stripped_msg)
+    return stripped_msg
 
 # Run local function with the same name as the action
 action = locals().get('action_' + options.action)
