8ad847f9974e1a0bc1b52bf7a7f978f6b5652448
Anders Vaage <anders@uib.no>
2018-09-04 15:42:30 +0200

    a simple cli for managing the status api (basically for hassle-free get and delete)

---
+++ manage_status.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+from himlarcli.parser import Parser
+from himlarcli.status import Status
+from himlarcli import utils as himutils
+
+himutils.is_virtual_env()
+
+parser = Parser()
+options = parser.parse_args()
+status = Status(options.config, debug=options.debug)
+status.set_dry_run(options.dry_run)
+
+def action_list():
+    status.list_status()
+
+def action_delete():
+    status_id = options.status_id
+    if not himutils.confirm_action('Are you sure you want to delete status message with id %s' % status_id):
+        return
+    status.delete_status(status_id)
+
+action = locals().get('action_' + options.action)
+if not action:
+    himutils.sys_error("Function action_%s() not implemented" % options.action)
+action()
