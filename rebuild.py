9da735aa774e5d066220bfcdee5c2b0a536e9b41
Anders Vaage <anders@unix.uib.no>
2018-09-11 14:52:03 +0200

    silence a host in sensu when rebuilding with rebuild.py

--- rebuild.py
+++ rebuild.py
@@ -2,11 +2,14 @@
 import utils
 import sys
 from himlarcli.foremanclient import Client
+from himlarcli.sensu import Sensu
 
 desc = 'Toggle rebuild host in foreman'
 options = utils.get_options(desc)
 
 foreman = Client(options.config, options.debug)
+sensu = Sensu(options.config, debug=options.debug)
 
 for host in options.host:
+    sensu.silence_host(host, expire=3600)
     foreman.set_host_build(host)
