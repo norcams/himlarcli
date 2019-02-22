5a2223d1e2e1e30491e5929dc9f53c2b46167249
tanzr <tannazn@mail.uio.no>
2019-02-22 12:27:07 +0100

    outsending works well

--- mail.py
+++ mail.py
@@ -18,9 +18,10 @@ ksclient = Keystone(options.config, debug=options.debug)
 ksclient.set_dry_run(options.dry_run)
 logger = ksclient.get_logger()
 
+# Update these before running the script
 emails_file = 'notify/user_emails.txt'
-body_content = 'notify/mailto_all.txt'
-subject = 'test123'
+content = 'notify/mailto_all.txt'
+subject = 'INFO UH-IaaS'
 
 
 if hasattr(options, 'region'):
@@ -31,85 +32,31 @@ else:
 if not regions:
     himutils.sys_error('no valid regions found!')
 
-
-
 def action_file():
-    if options.template: #if template is given
-        body_content = options.template
-        email_content = open(body_content, 'r')
-        content = email_content.read()
-        if options.dry_run:
-            print content
-            #logger.debug('=> DRY-RUN: print out the content %s' % content)
-        # else: send mail
-    email_content.close()
-    if not options.template:
-        email_content = open(body_content, 'r')
-        content = email_content.read()
+    if options.template:
+        content = options.template
+        email_content = open(content, 'r')
+        body_content = email_content.read()
         if options.dry_run:
             print content
             #logger.debug('=> DRY-RUN: print out the content %s' % content)
         else:
+            print "jooooo"
             mail = Mail(options.config, debug=options.debug)
             mail.set_dry_run(options.dry_run)
             msg = MIMEText(body_content, 'plain')
-            msg['subject'] = '123456'
-            print "heer 2"
+            msg['subject'] = subject
+            print msg
             with open(emails_file, 'r') as emails:
                 for toaddr in emails.readlines():
-                    print "heer 3"
                     try:
                         logger.debug('=> Sending email ...')
-                        print "try"
-                        #mail.send_mail(toaddr, msg, fromaddr='noreply@usit.uio.no')
+                        #print msg
+                        mail.send_mail(toaddr, msg, fromaddr='noreply@usit.uio.no')
                     except ValueError:
                         himutils.sys_error('Not able to send the email.')
             emails.close()
     email_content.close()
-
-
-    # ToDo
-    # if -t use template, otherwise use default
-
-def action_instance():
-    for region in regions:
-        novaclient = Nova(options.config, debug=options.debug, log=logger, region=region)
-        instances = novaclient.get_instances()
-        mapping = dict(region=region.upper())
-        body_content = himutils.load_template(inputfile=options.template,
-                                                mapping=mapping,
-                                                log=logger)
-        subject = options.subject
-        mail.set_keystone_client(ksclient)
-        mail.set_dry_run(options.dry_run)
-    
-        users = mail.mail_instance_owner(instances, body_content, subject)
-        print(users)
-
-        # users = mail.mail_instance_owner(instances=instances,
-        #                                     body=body_content,
-        #                                     subject=subject,
-        #                                     admin=True,
-        #                                     options=['project', 'az'])
-
-def action_project():
-    mail = Mail(options.config, debug=options.debug)
-    if options.type:
-        search_filter['type'] = options.type
-    projects = ksclient.get_projects(domain=options.domain, **search_filter)
-    for project in projects:
-        if not options.filter or options.filter in project.name:
-            instances = novaclient.get_project_instances(project.id)
-            mapping = dict(region=region.upper(), project=project.name)
-            body_content = himutils.load_template(inputfile=options.template,
-                                                  mapping=mapping,
-                                                  log=logger)
-            subject = ('UH-IaaS: Your instances will be terminated (%s)' % (region))
-
-            mail.set_keystone_client(ksclient)
-            mail.set_dry_run(options.dry_run)
-            users = mail.mail_instance_owner(instances, body_content, subject)
-            print users
 
 # Run local function with the same name as the action
 action = locals().get('action_' + options.action)
