235630dac59c13b25dd77e1f4c76635e000a2a91
tanzr <tannazn@mail.uio.no>
2019-02-28 17:14:52 +0100

    send mail to all & type project

--- mail.py
+++ mail.py
@@ -15,9 +15,7 @@ options = parser.parse_args()
 printer = Printer(options.format)
 
 ksclient = Keystone(options.config, debug=options.debug)
-ksclient.set_dry_run(options.dry_run)
 logger = ksclient.get_logger()
-novaclient = Nova(options.config, debug=options.debug, log=logger)
 
 # Update these before running the script
 emails_file = 'notify/user_emails.txt'
@@ -47,12 +45,10 @@ def action_file():
         email_content = open(content, 'r')
         body_content = email_content.read()
         if options.dry_run:
-            print content
-            #logger.debug('=> DRY-RUN: print out the content %s' % content)
+            print body_content
         else:
             mail = Mail(options.config, debug=options.debug)
-            mail.set_dry_run(options.dry_run)
-            msg = MIMEText(body_content, 'plain')
+            msg = MIMEText(body_content)
             msg['subject'] = subject
             print msg
             with open(emails_file, 'r') as emails:
@@ -64,46 +60,79 @@ def action_file():
                         himutils.sys_error('Not able to send the email.')
             emails.close()
     email_content.close()
+    mail.close()
 
-
-# Check nova version
 def action_instance():
-    email_content = open(content, 'r')
-    body_content = email_content.read()
-    for region in regions:
-        novaclient = Nova(options.config, debug=options.debug, log=logger) #, region=region)
-        instances = novaclient.get_instances()
-        mail = Mail(options.config, debug=options.debug)
-        mail.set_keystone_client(ksclient)
-        users = mail.mail_instance_owner(instances=instances,
-                                           body=body_content,
-                                           subject=subject,
-                                           admin=True)
-    email_content.close()
+    if options.template:
+        content = options.template
+        email_content = open(content, 'r')
+        body_content = email_content.read()
+        if options.dry_run:
+            print body_content
+        else:
+            with open(content, 'r') as email_content:
+                body_content = email_content.read()
+            for region in regions:
+                novaclient = himutils.get_client(Nova, options, logger, region)
+                instances = novaclient.get_instances()
+                mail = Mail(options.config, debug=options.debug)
+                mail.set_keystone_client(ksclient)
+                users = mail.mail_instance_owner(instances=instances,
+                                                   body=body_content,
+                                                   subject=subject,
+                                                   admin=True)
+    mail.close()
 
 def action_project():
-    email_content = open(content, 'r')
-    body_content = email_content.read()
-    mail = Mail(options.config, debug=options.debug)
-    search_filter = dict()
-    projects = ksclient.get_projects(domain=options.domain, **search_filter)
-    if options.filter and options.filter != 'all':
-        search_filter['type'] = options.filter
-    if options.type:
-        search_filter['type'] = options.type
-    for project in projects:
-        project_type = project.type if hasattr(project, 'type') else '(unknown)'
-#        if not options.filter or options.filter in project.name:
-        instances = novaclient.get_project_instances(project.id)
-        mail.set_keystone_client(ksclient)
-        users = mail.mail_instance_owner(instances=instances,
-                                           body=body_content,
-                                           subject=subject,
-                                           admin=True)
-    email_content.close()
+    if options.template:
+        content = options.template
+        email_content = open(content, 'r')
+        body_content = email_content.read()
+        if options.dry_run:
+            print body_content
+        else:
+            with open(content, 'r') as email_content:
+                body_content = email_content.read()
+            mail = Mail(options.config, debug=options.debug)
+            search_filter = dict()
+            projects = ksclient.get_projects(domain=options.domain, **search_filter)
+            if options.filter and options.filter != 'all':
+                search_filter['type'] = options.filter
+            if options.type:
+                search_filter['type'] = options.type
+            for project in projects:
+                project_type = project.type if hasattr(project, 'type') else '(unknown)'
+                # if not options.filter or options.filter in project.name:
+                instances = novaclient.get_project_instances(project.id)
+                mail.set_keystone_client(ksclient)
+                users = mail.mail_instance_owner(instances=instances,
+                                                   body=body_content,
+                                                   subject=subject,
+                                                   admin=True)
+    mail.close()
 
-# Run local function with the same name as the action
-action = locals().get('action_' + options.action)
+def action_sendtoall():
+    if options.template:
+        content = options.template
+        email_content = open(content, 'r')
+        body_content = email_content.read()
+        if options.dry_run:
+            print body_content
+        else:
+            with open(content, 'r') as email_content:
+                body_content = email_content.read()
+            projects = ksclient.list_projects('Dataporten')
+            for project in projects:
+                mail = Mail(options.config, debug=options.debug)
+                msg = MIMEText(body_content)
+                msg['subject'] = subject
+                mail.send_mail(project, msg, fromaddr='noreply@uh-iaas.no')
+                print '\nProject: %s' % project
+    mail.close()
+
+
+# Run local function with the same name as the action (Note: - => _)
+action = locals().get('action_' + options.action.replace('-', '_'))
 if not action:
     himutils.sys_error("Function action_%s() not implemented" % options.action)
 action()
