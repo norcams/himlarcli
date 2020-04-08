9aeb7cff8c45147e98da16eacdc897006d20a485
Raymond Kristiansen <raymond.kristiansen@it.uib.no>
2020-04-08 13:48:50 +0200

    Rewritten glance client and image script

--- himlarcli/glance.py
+++ himlarcli/glance.py
@@ -2,7 +2,6 @@ import itertools
 from himlarcli.client import Client
 from glanceclient import Client as glclient
 from glanceclient import exc
-import sys
 
 class Glance(Client):
 
@@ -34,10 +33,12 @@ class Glance(Client):
             self.log_error('Image with ID %s not found!' % image_id)
         return image
 
-    """ Get images.
-    To filter use filters dict with key value pairs.
-    E.G. get_images(filters={'key': 'value'}) """
     def get_images(self, **kwargs):
+        """
+            Get images.
+            To filter use filters dict with key value pairs.
+            E.G. get_images(filters={'key': 'value'})
+        """
         return self.client.images.list(**kwargs)
 
     def find_image(self, **kwargs):
@@ -48,7 +49,7 @@ class Glance(Client):
         try:
             images = list(result) if result else list()
             return images
-        except exc.HTTPServiceUnavailable as e:
+        except exc.HTTPServiceUnavailable:
             self.log_error('Glance: Service Unavailable')
         return list()
 
@@ -67,10 +68,19 @@ class Glance(Client):
         return None
 
     def create_image(self, source_path, **kwargs):
-        self.logger.debug('=> create new image %s' % kwargs['name'])
-        self.image = self.client.images.create(**kwargs)
-        self.upload_image(source_path)
-        return self.image
+        if 'name' not in kwargs:
+            self.log_error('image name missing!')
+            return None
+        self.debug_log('create new image {}'.format(kwargs['name']))
+        try:
+            if not self.dry_run:
+                image = self.client.images.create(**kwargs)
+        except exc.HTTPServiceUnavailable as e:
+            self.log_error(e)
+        if image:
+            self.__upload_image(source_path, image)
+            return image
+        return None
 
     def delete_private_images(self, project_id):
         images = self.find_image(filters={'owner': project_id})
@@ -78,29 +88,62 @@ class Glance(Client):
             self.delete_image(image.id)
 
     def delete_image(self, image_id):
-        if not self.image and not image_id:
-            self.logger.critical('Image must exist before deletion.')
+        if not self.get_image_by_id(image_id):
+            self.debug_log('deletion failed! not fond: {}'.format(image_id))
             return
-        if not image_id:
-            image_id = self.image.id
-        self.debug_log('image delete %s' % image_id)
+        self.debug_log('delete image id {}'.format(image_id))
         try:
             if not self.dry_run:
                 self.client.images.delete(image_id)
         except exc.HTTPServiceUnavailable as e:
             self.log_error(e)
 
-    def update_image(self, name, image_id=None, **kwargs):
-        if not self.image and not image_id:
-            self.logger.debug('=> image not found %s' % name)
-            if name:
-                self.get_image(name)
-            else:
-                self.logger.critical('Image must exist before upload.')
-                sys.exit(1)
-        if not image_id:
-            image_id = self.image.id
-        self.client.images.update(image_id=image_id, name=name, **kwargs)
+    def update_image(self, name, image_id, **kwargs):
+        if not self.get_image_by_id(image_id):
+            self.debug_log('image not fond: {}'.format(name))
+            return
+        try:
+            if not self.dry_run:
+                self.client.images.update(image_id=image_id, name=name, **kwargs)
+        except exc.HTTPServiceUnavailable as e:
+            self.log_error(e)
+
+    def get_image_access(self, image_id):
+        try:
+            members = self.client.image_members.list(image_id)
+        except exc.HTTPServiceUnavailable as e:
+            self.log_error(e)
+        return members
+
+    def set_image_access(self, image_id, project_id, action='grant'):
+        if action not in ['grant', 'revoke']:
+            self.debug_log('unknown image access action: {}'.format(action))
+            return
+
+        if action == 'grant':
+            if self.__get_project_image_member(image_id, project_id):
+                self.debug_log(('membership exsist: dropping grant for project '
+                    + '{} on image {}').format(project_id, image_id))
+                return
+            if not self.dry_run:
+                try:
+                    self.client.image_members.create(image_id, project_id)
+                    self.client.image_members.update(image_id, project_id, 'accepted')
+                except exc.HTTPServiceUnavailable as e:
+                    self.log_error(e)
+            self.debug_log('grant access to project {} for image {}'.
+                           format(project_id, image_id))
+        elif action == 'revoke':
+            if not self.__get_project_image_member(image_id, project_id):
+                return
+            if not self.dry_run:
+                try:
+                    self.client.image_members.delete(image_id, project_id)
+                except exc.HTTPServiceUnavailable as e:
+                    self.log_error(e)
+            self.debug_log('revoke access to project {} for image {}'.
+                           format(project_id, image_id))
+
 
     def set_access(self, image_id, project_id, action='create'):
         if action not in ['create', 'delete']:
@@ -129,41 +172,36 @@ class Glance(Client):
     def get_access(self, image_id):
         return self.client.image_members.list(image_id)
 
-    def deactivate(self, name=None, image_id=None):
-        if not self.image and not image_id:
-            self.logger.debug('=> image not found %s' % name)
-            if name:
-                self.get_image(name)
-            else:
-                self.logger.critical('Image must exist to deactivate.')
-                sys.exit(1)
-        if not image_id:
-            image_id = self.image.id
-        self.client.images.deactivate(image_id)
-        self.logger.debug('=> deactivate image id %s' % image_id)
+    def deactivate(self, image_id):
+        if not self.get_image_by_id(image_id):
+            self.debug_log('deactivation failed! not fond: {}'.format(image_id))
+            return
+        self.debug_log('deactivate image id {}'.format(image_id))
+        try:
+            if not self.dry_run:
+                self.client.images.deactivate(image_id)
+        except exc.HTTPServiceUnavailable as e:
+            self.log_error(e)
 
     def reactivate(self, image_id):
+        if not self.get_image_by_id(image_id):
+            self.debug_log('reactiation failed! not fond: {}'.format(image_id))
+            return
+        self.debug_log('reactivate image id {}'.format(image_id))
         try:
-            self.logger.debug('=> reactivate image id %s' % image_id)
-            self.client.images.reactivate(image_id)
-        except exc.HTTPNotFound:
-            self.logger.error('=> image id %s not found' % image_id)
+            if not self.dry_run:
+                self.client.images.reactivate(image_id)
+        except exc.HTTPServiceUnavailable as e:
+            self.log_error(e)
 
-    def upload_image(self, source_path, name=None):
-        if not self.image:
-            self.logger.debug('=> image not found %s' % name)
-            if name:
-                self.get_image(name)
-            else:
-                self.logger.critical('Image must exist before upload.')
-                sys.exit(1)
+    def __upload_image(self, source_path, image):
         try:
-            self.client.images.upload(self.image.id, open(source_path, 'rb'))
-            self.logger.debug('=> upload new image %s' % source_path)
+            if not self.dry_run:
+                self.client.images.upload(image.id, open(source_path, 'rb'))
+            self.debug_log('upload new image %s' % source_path)
         except BaseException as e:
             print e
-            self.logger.critical('Upload of %s failed' % self.image.name)
-            sys.exit(1)
+            self.log_error('Upload of {} failed'.format(image.name), 1)
 
     @staticmethod
     def find_optimal_flavor(image, flavors):
@@ -172,6 +210,13 @@ class Glance(Client):
             if flavor.ram >= image['min_ram'] and flavor.disk >= image['min_disk']:
                 return flavor
         return None
+
+    def __get_project_image_member(self, image_id, project_id):
+        members = self.client.image_members.list(image_id)
+        for member in members:
+            if member.member_id == project_id:
+                return True
+        return False
 
     def __get_images(self):
         self.images = self.client.images.list()
