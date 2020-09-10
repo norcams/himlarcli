0c1cd76084e979cef5abe4835e55f174ae3eb25e
Raymond Kristiansen <raymond@nexusweb.no>
2020-09-10 11:58:50 +0200

    Updated dataporten script to use parser

--- dataporten.py
+++ dataporten.py
@@ -1,33 +1,48 @@
 #!/usr/bin/env python
-""" Setup dataporten openid mapping """
-
-import utils
 from himlarcli.keystone import Keystone
-from himlarcli import utils as himutils
+from himlarcli.parser import Parser
+from himlarcli import utils
 
-options = utils.get_options('Setup dataporten openid mapping',
-                             hosts=0, dry_run=True)
+utils.is_virtual_env()
+
+# Load parser config from config/parser/*
+parser = Parser()
+options = parser.parse_args()
+
 ksclient = Keystone(options.config, debug=options.debug)
 ksclient.set_dry_run(options.dry_run)
-ksclient.set_domain('dataporten')
+ksclient.set_domain(options.domain)
+
 # Domain should be create from hieradata
 domain = ksclient.get_domain_id()
+
 rules = [{
     "local": [{
-        "user": { "name": "{0}", "id": "{0}" },
-        "group": { "name": "{0}-group", "domain": { "id": domain } } }],
-    "remote": [{ "type": "OIDC-email" }, { "type": "OIDC-name" }]
+        "user": {"name": "{0}", "id": "{0}"},
+        "group": {"name": "{0}-group", "domain": {"id": domain}}}],
+    "remote": [{"type": "OIDC-email"}, {"type": "OIDC-name"}]
 }, {
     "local": [{
-        "group": { "name": "nologin", "domain": { "id": domain } } }],
-        "remote": [{ "type": "OIDC-email" }, { "type": "OIDC-name" }]
+        "group": {"name": "nologin", "domain":{"id": domain}}}],
+    "remote": [{"type": "OIDC-email"}, {"type": "OIDC-name"}]
 }]
 
 # Crate nologin group
-desc = 'All authenticated users are mapped to nologin which has no role grants'
-ksclient.create_group('nologin', desc, 'dataporten')
 
-# Create provider, mapping and container to connect them
-ksclient.set_identity_provider('dataporten', 'https://auth.dataporten.no', 'Federated user from Dataporten')
-ksclient.set_mapping('dataporten_personal', rules)
-ksclient.set_protocol('openid', 'dataporten', 'dataporten_personal')
+def action_update():
+
+    desc = 'All authenticated users are mapped to nologin which has no role grants'
+    ksclient.create_group('nologin', desc, 'dataporten')
+
+    # Create provider, mapping and container to connect them
+    ksclient.set_identity_provider('dataporten',
+                                   'https://auth.dataporten.no',
+                                   'Federated user from Dataporten')
+    ksclient.set_mapping('dataporten_personal', rules)
+    ksclient.set_protocol('openid', 'dataporten', 'dataporten_personal')
+
+# Run local function with the same name as the action (Note: - => _)
+action = locals().get('action_' + options.action.replace('-', '_'))
+if not action:
+    utils.sys_error("Function action_%s() not implemented" % options.action)
+action()
