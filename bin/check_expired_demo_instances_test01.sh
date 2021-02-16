b1a9a8570affedd0a4ecd407ee3a362c6cdddf9f
tanzr <tanzr@users.noreply.github.com>
2021-02-16 13:18:28 +0100

    added scripts for demo instances

---
+++ bin/check_expired_demo_instances_test01.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+source /opt/himlarcli/bin/activate
+/opt/himlarcli/demo.py expired -d 60 -t notify/demo-notify-expired-instances-30dleft.txt --region test01
+/opt/himlarcli/demo.py expired -d 75 -t notify/demo-notify-expired-instances-15dleft.txt --region test01
+/opt/himlarcli/demo.py expired -d 89 -t notify/demo-notify-expired-instances-1dleft.txt --region test01
