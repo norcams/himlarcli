cf6265b2705fd58de276b6edfee9373e44ab6055
tanzr <tanzr@users.noreply.github.com>
2021-03-05 08:23:24 +0100

    minor fix

--- bin/check_expired_demo_instances.sh
+++ bin/check_expired_demo_instances.sh
@@ -3,6 +3,6 @@
 # Run as check_expired_demo_instances.sh region
 region=$1
 source /opt/himlarcli/bin/activate
-/opt/himlarcli/demo.py expired -d 16 --region $region -t notify/demo-notify-expired-instances-30dleft.txt --force --debug
+/opt/himlarcli/demo.py expired -d 60 --region $region -t notify/demo-notify-expired-instances-30dleft.txt --force --debug
 /opt/himlarcli/demo.py expired -d 75 --region $region -t notify/demo-notify-expired-instances-15dleft.txt --force --debug
 /opt/himlarcli/demo.py expired -d 89 --region $region -t notify/demo-notify-expired-instances-1dleft.txt --force --debug
