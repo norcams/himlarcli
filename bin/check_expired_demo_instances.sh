c730d35fc31d7959669986c9f5a543b81466edd5
tanzr <tanzr@users.noreply.github.com>
2021-03-05 12:13:32 +0100

    dryrun

--- bin/check_expired_demo_instances.sh
+++ bin/check_expired_demo_instances.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-
 # Run as check_expired_demo_instances.sh region
+
 region=$1
 source /opt/himlarcli/bin/activate
-/opt/himlarcli/demo.py expired -d 60 --region $region -t notify/demo-notify-expired-instances-30dleft.txt --force --debug
-/opt/himlarcli/demo.py expired -d 75 --region $region -t notify/demo-notify-expired-instances-15dleft.txt --force --debug
-/opt/himlarcli/demo.py expired -d 89 --region $region -t notify/demo-notify-expired-instances-1dleft.txt --force --debug
+/opt/himlarcli/demo.py expired -d 60 --region $region -t notify/demo-notify-expired-instances-30dleft.txt --force --debug --dry-run
+/opt/himlarcli/demo.py expired -d 75 --region $region -t notify/demo-notify-expired-instances-15dleft.txt --force --debug --dry-run
+/opt/himlarcli/demo.py expired -d 89 --region $region -t notify/demo-notify-expired-instances-1dleft.txt --force --debug --dry-run
