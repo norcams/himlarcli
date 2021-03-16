b65aad9579d861febdc8072dfdeb20db4fb78638
tanzr <tanzr@users.noreply.github.com>
2021-03-16 17:24:35 +0100

    check scripts

--- bin/check_expired_demo_instances.sh
+++ bin/check_expired_demo_instances.sh
@@ -3,6 +3,6 @@
 
 region=$1
 source /opt/himlarcli/bin/activate
-/opt/himlarcli/demo.py expired -d 60 --region $region -t notify/demo-notify-expired-instances-30dleft.txt --force --debug --dry-run
-/opt/himlarcli/demo.py expired -d 75 --region $region -t notify/demo-notify-expired-instances-15dleft.txt --force --debug --dry-run
-/opt/himlarcli/demo.py expired -d 89 --region $region -t notify/demo-notify-expired-instances-1dleft.txt --force --debug --dry-run
+/opt/himlarcli/demo.py expired -d 60 --region $region -t notify/demo-notify-expired-instances.txt --force --debug --dry-run
+/opt/himlarcli/demo.py expired -d 75 --region $region -t notify/demo-notify-expired-instances.txt --force --debug --dry-run
+/opt/himlarcli/demo.py expired -d 89 --region $region -t notify/demo-notify-expired-instances.txt --force --debug --dry-run
--- bin/delete_expired_demo_instances.sh
+++ bin/delete_expired_demo_instances.sh
@@ -2,4 +2,4 @@
 
 region=$1
 source /opt/himlarcli/bin/activate
-/opt/himlarcli/demo.py delete_expired_instance -d 90 --region $region --force --debug 
+/opt/himlarcli/demo.py delete --region $region --force --debug --dry-run
