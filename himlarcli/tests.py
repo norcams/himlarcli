f9ba88c9ac625fac80504308a31022ae444cccb2
Raymond Kristiansen <raymond.kristiansen@uib.no>
2022-07-05 12:17:37 +0200

    updated virutal env test

--- himlarcli/tests.py
+++ himlarcli/tests.py
@@ -1,7 +1,7 @@
 import sys
 
 def is_virtual_env():
-    base_prefix = getattr(sys, "base_prefix", None) or getattr(sys, "real_prefix", None) or sys.prefix
+    base_prefix = getattr(sys, "real_prefix", None) or getattr(sys, "base_prefix", None) or sys.prefix
     if sys.prefix == base_prefix:
         print("Remember to source bin/activate!")
         sys.exit(1)
