db9fff1ede07eab5a7e7603e9a849e4718604d10
Raymond Kristiansen <raymond.kristiansen@uib.no>
2022-07-13 14:47:28 +0200

    updated request specs script

--- request_specs.py
+++ request_specs.py
@@ -9,6 +9,7 @@ from himlarcli import utils
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from nova.db.sqlalchemy import api_models
+import json
 
 parser = Parser()
 options = parser.parse_args()
@@ -24,9 +25,40 @@ Session = sessionmaker(bind=engine)
 session = Session()
 
 def action_show():
-    spec = session.query(api_models.RequestSpec).filter_by(instance_uuid=options.instance).first()
-    print(options.instance)
-    print(test)
+    request_spec = session.query(api_models.RequestSpec) \
+                .filter_by(instance_uuid=options.instance).first()
+    if request_spec:
+        logger.debug('=> found request spec for instance %s', options.instance)
+        spec = json.loads(request_spec.spec)
+        if 'scheduler_hints' in spec['nova_object.data']:
+            printer.output_dict(spec['nova_object.data']['scheduler_hints'])
+            printer.output_msg('NB! if this server group is delete we will '
+                               'need to remove the scheduler hints')
+        else:
+            printer.output_msg('no scheduler_hints found for instance')
+
+def action_remove_hints():
+    request_spec = session.query(api_models.RequestSpec) \
+                .filter_by(instance_uuid=options.instance).first()
+    if request_spec:
+        logger.debug('=> found request spec for instance %s', options.instance)
+        spec = json.loads(request_spec.spec)
+        if 'scheduler_hints' in spec['nova_object.data']:
+            del spec['nova_object.data']['scheduler_hints']
+            logger.debug('=> try to remove scheduler_hints from request spec')
+            q = ("Are you sure you will remove the scheduler hints for this "
+                "instance in the database? Please run backup first. Continue?")
+            if not utils.confirm_action(q):
+                return
+            request_spec.spec = json.dumps(spec)
+            if not options.dry_run:
+                session.commit()
+                printer.output_msg('scheduler_hints removed from instance')
+            else:
+                printer.output_msg(('DRY-RUN: scheduler_hints should '
+                                    'be removed from instance'))
+        else:
+            printer.output_msg('no scheduler_hints found for instance')
 
 # Run local function with the same name as the action (Note: - => _)
 action = locals().get('action_' + options.action.replace('-', '_'))
