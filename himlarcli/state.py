7666a7c182c673234f3b5c3c22032f2b342dcbed
Raymond Kristiansen <raymond.kristiansen@uib.no>
2022-07-15 11:23:36 +0200

    style(stateclient): pylint cleanup

--- himlarcli/state.py
+++ himlarcli/state.py
@@ -29,8 +29,10 @@ Base = declarative_base(cls=Resource)
 
 class State(Client):
 
+    """ State client to manage state data in sqllite """
+
     def __init__(self, config_path, debug, log=False):
-        super(State, self).__init__(config_path, debug, log)
+        super().__init__(config_path, debug, log)
         self.connect()
 
     def get_client(self):
@@ -40,28 +42,32 @@ class State(Client):
     def connect(self):
         """ Connect to the sqlite database """
         db = self.get_config('state', 'db')
-        self.engine = create_engine('sqlite:///%s' % db, poolclass=NullPool)
+        self.engine = create_engine(f'sqlite:///{db}', poolclass=NullPool)
         Base.metadata.bind = self.engine
         DBSession = sessionmaker()
         self.session = DBSession()
         Base.metadata.create_all(self.engine)
-        self.debug_log("sqlite driver %s" % self.engine.driver)
-        self.debug_log("connected to %s" % db)
+        self.logger.debug("=> sqlite driver %s", self.engine.driver)
+        self.logger.debug("=> connected to %s", db)
 
     def close(self):
+        self.logger.debug('=> close db session')
         self.session.close()
 
     def add(self, resource):
         """ Add a new resource to database """
-        self.debug_log('add new resource of type %s' % resource.to_str())
-        self.session.add(resource)
+        pf = self.log_prefix()
+        self.logger.debug('%s add new resource of type %s', pf, resource.to_str())
+        if not self.dry_run:
+            self.session.add(resource)
         self.session.commit()
 
     def update(self, resource, data):
         """ Update an resource with new data """
+        pf = self.log_prefix()
         if not self.dry_run:
             resource.update(data)
-        self.debug_log('update resource %s' % resource.to_str())
+        self.logger.debug('%s update resource %s', pf, resource.to_str())
 
     def get_all(self, class_name, **kwargs):
         return self.session.query(class_name).filter_by(**kwargs).all()
@@ -71,7 +77,7 @@ class State(Client):
 
     def purge(self, table):
         """ Drop a table for database """
-        self.debug_log('drop table %s' % table.title())
+        self.logger.debug('=> drop table %s', table.title())
         found_table = getattr(sys.modules[__name__], table.title())
         if not self.dry_run:
             found_table.__table__.drop()
@@ -81,6 +87,9 @@ class State(Client):
 #
 
 class Keypair(Base):
+
+    """ Keypair data model """
+
     __tablename__ = 'keypair'
     id = Column(Integer, primary_key=True)
     user_id = Column(String(63), nullable=False, index=True)
@@ -91,28 +100,35 @@ class Keypair(Base):
     public_key = Column(String(1024))
 
     def to_str(self):
-        return 'keys for user id %s' % self.user_id
+        return f'keys for user id {self.user_id}'
 
     def compare(self, attributes):
         pass
 
 class Instance(Base):
+
+    """ Instance data model """
+
     __tablename__ = 'instance'
     id = Column(Integer, primary_key=True)
     instance_id = Column(String(63), nullable=False, index=True)
     created = Column(DateTime, default=datetime.now)
     name = Column(String(255))
     aggregate = Column(String(255))
+    host = Column(String(255))
     status = Column(String(15))
     region = Column(String(15), index=True)
 
     def to_str(self):
-        return 'instance with id {}'.format(self.instance_id)
+        return f'instance with id {self.instance_id}'
 
     def compare(self, attributes):
         pass
 
 class Quota(Base):
+
+    """ Quota data model """
+
     __tablename__ = 'quota'
     id = Column(Integer, primary_key=True)
     project_id = Column(String(63), nullable=False, index=True)
@@ -131,15 +147,15 @@ class Quota(Base):
     security_groups = Column(Integer)
 
     def to_str(self):
-        return 'quota for project id %s' % self.project_id
+        return f'quota for project id {self.project_id}'
 
     def compare(self, attributes):
-        miss_match = dict()
+        miss_match = {}
         for k, v in attributes.iteritems():
             if k == 'id':
                 continue
             if k not in Quota.__dict__:
                 continue
             if getattr(self, k) != v:
-                miss_match[k] = '%s =! %s' % (getattr(self, k), v)
+                miss_match[k] = f'{getattr(self, k)} =! {v}'
         return miss_match
