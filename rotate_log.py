e0b099edd8b7dcca762fc73b58f231ed8f966c18
Raymond Kristiansen <raymond.kristiansen@uib.no>
2022-07-25 10:26:25 +0200

    fix: rotate file log script

--- rotate_log.py
+++ rotate_log.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python
 
+import logging
 from himlarcli import utils
 
+# config logger with logging.yaml
 logger = utils.setup_logger(__name__, False, './')
 logger.debug('rotating logs')
-logger.doRollover()
+
+# rotate file handler
+for handler in logging.getLogger().handlers:
+    if handler.name == 'file':
+        handler.doRollover()
