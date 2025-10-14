340ae1bf060edf05c75b20c65a94d057a607e2ba
Raymond Kristiansen <raymond.kristiansen@uib.no>
2025-10-14 08:50:21 +0200

    fix: drop compute stats since it is not working

--- bin/gen_stats.sh
+++ bin/gen_stats.sh
@@ -2,7 +2,7 @@
 
 source /opt/himlarcli/bin/activate
 /opt/himlarcli/stats.py legacy --quiet
-/opt/himlarcli/stats.py compute --quiet
+#/opt/himlarcli/stats.py compute --quiet
 
 /opt/himlarcli/stats.py legacy --quiet -c /etc/himlarcli/config.ini.oldstats
-/opt/himlarcli/stats.py compute --quiet -c /etc/himlarcli/config.ini.oldstats
+#/opt/himlarcli/stats.py compute --quiet -c /etc/himlarcli/config.ini.oldstats
