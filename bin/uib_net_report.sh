7746f530f36438799907570b48396a8059ac7491
Raymond Kristiansen <raymond.kristiansen@uib.no>
2025-12-23 10:35:06 +0100

    feat: update uib net report script

--- bin/uib_net_report.sh
+++ bin/uib_net_report.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 source /opt/himlarcli/bin/activate
-/opt/himlarcli//net_report.py mail --region bgo -e raymond.kristiansen@uib.no -s '[NREC] TESTING'
+/opt/himlarcli//net_report.py mail --region bgo -e Jimmy.Thomsen@uib.no -s '[NREC] Automatisk oversikt'
