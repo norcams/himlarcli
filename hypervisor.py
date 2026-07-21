6249be8e02ca7f0738109a0080f483de6be0303b
Trond Hasle Amundsen <trondham@uio.no>
2026-07-21 14:13:23 +0200

    fix regexp

--- hypervisor.py
+++ hypervisor.py
@@ -256,7 +256,7 @@ def action_list():
                 else:
                     r_vm_states += f"{Color.dim}-{Color.reset}"
 
-                if re.search(r"^(central1|windows1|placeholder1|hpc1|dedicated1)$", r_aggregate):
+                if re.search(r"central1|windows1|placeholder1|hpc1|dedicated1", r_aggregate):
                     # aggregates that use central disk
                     r_disk = "-"
                 else:
